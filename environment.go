9983b0ae0dde5bcd6a67743f885c30fd21112643
Christopher J. Ruwe <cjr@cruwe.de>
2019-02-19 17:03:43 +0000

    enable armclient to deal with Azure German Cloud

--- environment.go
+++ environment.go
@@ -1,31 +1,52 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 )
 
 type Environment struct {
 	aadLoginUrl string
+	apiVersion  string
 	armUrl      string
+	httpTransport http.Transport
 }
 
 const (
 	PublicEnvironmentName = "Public"
+	GermanEnvironmentName = "AzureGermanCloud"
 )
 
 var (
 	// Public Azure
 	publicAzureEnvironment = Environment{
-		aadLoginUrl: "https://login.microsoftonline.com",
-		armUrl:      "https://management.azure.com",
+		aadLoginUrl:   "https://login.microsoftonline.com",
+		apiVersion:    "2017-08-01",
+		armUrl:        "https://management.azure.com",
+		httpTransport: http.Transport{},
+	}
+
+	germanAzureEnvironment = Environment{
+		aadLoginUrl: "https://login.microsoftonline.de",
+		apiVersion: "2016-06-01",
+		armUrl:      "https://management.microsoftazure.de",
+		httpTransport: http.Transport{
+			TLSClientConfig: &tls.Config{
+				MaxVersion:    tls.VersionTLS11,
+				Renegotiation: tls.RenegotiateFreelyAsClient,
+			},
+		},
 	}
 )
 
 func getCurrentEnvironment(environmentName string) *Environment {
 	if strings.EqualFold(environmentName, PublicEnvironmentName) {
 		return &publicAzureEnvironment
+	} else if strings.EqualFold(environmentName, GermanEnvironmentName) {
+		return &germanAzureEnvironment
 	}
 
 	log.Fatalf("Unknown environment: %s", environmentName)
